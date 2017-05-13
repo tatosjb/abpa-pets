385b38616af33d5b1e348f5789ca5672331c92ad
Altair J. Fernandes <tatosjb@gmail.com>
2017-05-12 23:36:06 -0300

    Remove React

--- app/assets/javascripts/components/animals.jsx
+++
@@ -1,6 +0,0 @@
-alert(document.getElementById('my_app'));
-ReactDOM.render(
-    <h1>TESTE</h1>,
-    document.getElementById('my_app')
-);
-// alert("Funcionando");
--- app/assets/javascripts/third-party.js
+++ app/assets/javascripts/third-party.js
@@ -1,4 +1,2 @@
 //= require jquery/dist/jquery
 //= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
-//= require react/dist/react
-//= require react-dom/dist/react-dom
