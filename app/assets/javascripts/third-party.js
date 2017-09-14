18585ce492cb1364e81c743000f9fb970e4c9d05
Altair J. Fernandes <tatosjb@gmail.com>
2017-09-13 23:25:16 -0300

    Tootled navbar

--- app/assets/javascripts/third-party.js
+++ app/assets/javascripts/third-party.js
@@ -1,5 +1,3 @@
 //= require jquery/dist/jquery
 //= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
 //= require i18n/translations
-//= require react/dist/react
-//= require react-dom/dist/react-dom
