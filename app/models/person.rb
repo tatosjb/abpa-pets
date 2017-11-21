98088b599fe98fcb32272168972180789185bfad
Altair J. Fernandes <tatosjb@gmail.com>
2017-11-21 21:56:17 -0200

    Add pagination to people page

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,6 @@
 class Person < ApplicationRecord
   validates :name, :ibge_code, :address, :neighborhood, presence: true
-
   validates :cpf, presence: true, uniqueness: true
+
+  self.per_page = 10
 end
