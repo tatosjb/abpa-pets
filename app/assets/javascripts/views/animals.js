e034facb8c02667166029f7dae7bef51a2f87591
Altair J. Fernandes <tatosjb@gmail.com>
2017-12-09 00:23:49 -0200

    Fix set volunteer to animal bug

--- app/assets/javascripts/views/animals.js
+++ app/assets/javascripts/views/animals.js
@@ -30,7 +30,7 @@ animals = function () {
         newAnimal.description = $('#' + prefix + '-description').val();
         newAnimal.castred = $('#' + prefix + '-castred').is(":checked");
         newAnimal.placement = $('#' + prefix + '-placement').val();
-        newAnimal.person_id = $('#' + prefix + '-volunteer').val();
+        newAnimal.person_id = $('#' + prefix + '-person').val();
 
         console.log(newAnimal);
         return newAnimal;
